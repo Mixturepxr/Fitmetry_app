a017ad4d3feeeed5ad12e508c8d4ada5bdf9b271
Mixturepxr <73434312+Mixturepxr@users.noreply.github.com>
2022-11-21 15:10:33 +0700

    Update home_screen.dart

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,12 +1,10 @@
 import 'package:fitmetry/screen/home/components/body.dart';
-import 'package:fitmetry/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(backgroundColor: Colors.white),
+    return const Scaffold(
       body: Body(),
     );
   }
